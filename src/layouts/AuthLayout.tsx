8f92b0ee30cd80af31de6b1c036f84f3c5cb9de9
Gideon Assafuah <gassafuah@gmail.com>
2026-04-29 14:43:38 -0400

    add fonts to project

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
+import "../assets/styles/auth-layout.css";
 
 const AuthLayout: React.FC = () => {
-
   return (
-    <div className="container">
+    <div className="auth-layout">
+      <div className="container">
         <Outlet />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
+import "../assets/styles/default-layout.css";
 
 const DefaultLayout: React.FC = () => {
   return (
-    <div className="container">
-     {/*  TODO: Place header component here */}
+    <div className="default-layout">
+      <div className="container">
+        {/*  TODO: Place header component here */}
 
-      <Outlet />
-      
-      {/* TODO: Place footer component here */}
+        <Outlet />
+
+        {/* TODO: Place footer component here */}
+      </div>
     </div>
   );
 };
